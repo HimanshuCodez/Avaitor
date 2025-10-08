b93f30ec7a1598a0a882541fbebf47808f2b96e3
Himanshu gaur <himanshugaur055@gmail.com>
2025-10-08 17:29:25 +0530

    fixed?

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Home from './components/Home';
+
 import About from './components/About';
 import Contact from './components/Contact';
 import AviatorsGame from './Pages/Home';
@@ -11,7 +11,7 @@ import AddCash from './Pages/AddCash';
 function App() {
   return (
     <div>
-
+      <Navbar />
       <Routes>
         <Route path="/" element={<AviatorsGame />} />
         <Route path="/screen" element={<AviatorsGame />} />
